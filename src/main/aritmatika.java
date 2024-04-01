b84eabf304fee08692ece29230c5d1aff3674bac
MuhammadLuqman <mochhakim52@gmail.com>
2024-04-01 04:32:56 -0800

    test gitdeks

---
+++ src/main/aritmatika.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.Java_Pemula;
+
+/**
+ *
+ * @author Luqman
+ */
+public class aritmatika {
+    
+    public static void main(String argsp[])
+    {
+        int a = 10;
+        int b = 20;
+        int c = 30;
+        int d = 40;
+        
+        System.out.println("a + b = "+ (a + b));
+        System.out.println("a - b = "+ (a - b));
+        System.out.println("a * b = "+ (a * b));
+        System.out.println("b / a = "+ (b / a));
+        System.out.println("b + a = "+ (a % b));
+        System.out.println("c % a = "+ (c % a));
+        System.out.println("++a = "+ (++a));
+        System.out.println("--b = "+ (--b));
+        
+        System.out.println("c++ = "+ (++c));
+        System.out.println("++d = "+ (++d));
+        
+        
+    }    
+}
